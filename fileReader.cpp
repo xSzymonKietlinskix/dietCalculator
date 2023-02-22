4bf291d2817c2a5fe16dc48f83139dee94575db6
xSzymonKietlinskix <szymon.kietlinski@gmail.com>
2023-02-22 09:17:17 +0100

    Added new files

---
+++ fileReader.cpp
@@ -0,0 +1,3 @@
+#include "fileReader.hpp"
+
+
---
+++ fileReader.hpp
@@ -0,0 +1,9 @@
+#ifndef _fileReader
+#define _fileReader
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+#endif // !_fileReader
+
+using namespace std;
