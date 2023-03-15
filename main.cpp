a82a24e5889911059e3086be914327745598b6ae
xSzymonKietlinskix <szymon.kietlinski@gmail.com>
2023-03-15 19:46:26 +0100

    created new buttons

--- main.cpp
+++ main.cpp
@@ -2,78 +2,126 @@
 #include "dataBase.hpp"
 #include "imGui/imgui.h"
 #include "imGUi/imgui-SFML.h"
+//#include "products.hpp"
 
 #include <SFML/Graphics/CircleShape.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/System/Clock.hpp>
 #include <SFML/Window/Event.hpp>
 
+
 using namespace std;
 
-void dataBaseMenu(dataBase& dB, bool &isClicked) {
+class flags {
+public:
+	bool menu;
+	bool NewRecord;
+	bool butDataBaseMenu;
+	bool butAddNewRecord;
+	bool butConfirm;
+	bool countDiet;
+	void toggle(bool& stat);
+	flags() : butDataBaseMenu(false), butAddNewRecord(false), menu(true), NewRecord(false), butConfirm(false), countDiet(false){};
+};
 
-	if (ImGui::Button("Creat new table"))
+void flags::toggle(bool& stat) {
+	if (stat == true)
+		stat = false;
+	else
+		stat = true;
+}
+
+
+void dataBaseMenu(dataBase& dB, flags &fl) {
+	if (ImGui::Button("Creat new table", ImVec2(300, 50)))
 		dB.creatDefaultTable();
-	if (ImGui::Button("Add new record")) {
-		isClicked = true;
+	if (ImGui::Button("Add new record", ImVec2(300, 50))) {
+		fl.toggle(fl.butAddNewRecord);
+		cout << "clicked" << endl;
+	}
+	if (fl.butAddNewRecord == true) {
+		ImGui::Begin("Add new record",&fl.butAddNewRecord, ImGuiWindowFlags_NoMove);
+		static char t[128] = "";
+		static char n[128] = "";
+		static char p[20] = "";
+		static char c[20] = "";
+		ImGui::InputText("type",t, sizeof(t));
+		ImGui::InputText("name", n, sizeof(n));
+		ImGui::InputText("price", p, sizeof(p));
+		ImGui::InputText("cal", c, sizeof(c));
+		if (ImGui::Button("Confirm", ImVec2(300, 50))) {
+			cout << t << endl << n << endl << p << endl << c << endl;;
+			fl.toggle(fl.butConfirm);
+		}
+		if (fl.butConfirm) {
+			product pr(t, n, stof(c), stof(p));
+			dB.addSingleRecord(pr);
+			fl.toggle(fl.butConfirm);
+		}
 		ImGui::End();
 	}
-	cout << isClicked;
-	if (isClicked) {
-		
-		ImGui::Begin("test");
-		if (ImGui::Button("Creat new table"))
-			cout << "works";
-		ImGui::End();
-	}
+}
+
+void countDiet(dataBase& dB, flags& fl) {
+	;
 }
 
 int main(void) {
 	const char* location = "dataBase/newDataBase.db";
 	dataBase dB(location);
-	bool isClicked = false;
+	flags fl;
 
-
-	sf::RenderWindow window(sf::VideoMode(1280, 720), "Diet calculator");
+	sf::RenderWindow window(sf::VideoMode(2000, 1000), "Diet calculator");
 	window.setFramerateLimit(60);
 
 	ImGui::SFML::Init(window);
-
-	enum menuOptions { none, dataBaseOptions };
+	ImGuiStyle* style = &ImGui::GetStyle();
+	style->FramePadding = ImVec2(4, 2);
+	//style->Colors[ImGuiCol_WindowBg] = ImVec4(255, 255, 0, 192);
+	//style->Colors[ImGuiCol_Button] = ImVec4(255, 0, 0, 192);
+	ImGuiIO& io = ImGui::GetIO();
+	io.Fonts->AddFontDefault();
+	ImFont * defFont = io.Fonts->AddFontFromFileTTF("C:\\Windows\\Fonts\\Arial.ttf", 18.5f, NULL, io.Fonts->GetGlyphRangesJapanese());
+	IM_ASSERT(defFont != NULL);
+	style->WindowRounding = 0.40;
+	
 
 	sf::Clock deltaClock;
 	while (window.isOpen()) {
 		sf::Event event;
 		while (window.pollEvent(event)) {
 			ImGui::SFML::ProcessEvent(window, event);
-
-			if (event.type == sf::Event::Closed) {
+			if (event.type == sf::Event::Closed)
 				window.close();
-			}
 		}
-
+		
 		ImGui::SFML::Update(window, deltaClock.restart());
-		ImGui::Begin("Menu");
-		ImGui::Columns(2);
-		ImGui::SetColumnOffset(1, 230);
-		int choosenButton = none;
-		bool isSelceted;
-		ImGui::Checkbox("Data Base options", &isSelceted);
-		if (isSelceted == true)
-			choosenButton = dataBaseOptions;
-		else
-			choosenButton = none;
-		ImGui::NextColumn();
-		switch (choosenButton) {
-		case none:
-			break;
-		case dataBaseOptions:
-			dataBaseMenu(dB, isClicked);
-			break;
-		default:
-			break;
 
+		//***menu window***//
+		//**left column**//
+		if (fl.menu) {
+
+			ImGui::Begin("Menu", &fl.menu, ImGuiWindowFlags_NoMove);
+			ImGui::Columns(2);
+			ImGui::SetColumnOffset(1, 430);
+
+			//ImGui::PushFont(defFont);
+			if (ImGui::Button("Data base menu",ImVec2(300,50)))
+				fl.toggle(fl.butDataBaseMenu);
+			if (ImGui::Button("Count diet", ImVec2(300, 50))) {
+				fl.toggle(fl.countDiet);
+			}
+			//ImGui::PopFont();
 		}
+		//ImGui::PopFont();
+		
+		//**right column**//
+
+		ImGui::NextColumn();
+		if (fl.butDataBaseMenu)
+			dataBaseMenu(dB, fl);
+		if (fl.countDiet)
+			countDiet(dB, fl);
 
 
 		window.clear();
